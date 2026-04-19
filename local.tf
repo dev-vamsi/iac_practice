fc587a6a78e39c41c5aced6c3adf025d03c034e9
Vamsi Krishna <iam.vamsitupakula@gmail.com>
2026-04-19 10:52:44 +0530

    add random_pet resource to local.tf and update .terraform.lock.hcl

--- local.tf
+++ local.tf
@@ -2,3 +2,9 @@ resource "local_file" "pet" {
   filename = "${path.module}/pet.txt"
   content  = "This is a pet file created by Terraform - Updated content!"
 }
+
+resource "random_pet" "my_pet" {
+  length    = 2
+  separator = "-"
+  prefix    = "Mr."
+}
